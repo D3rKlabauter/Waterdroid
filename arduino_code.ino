35269c22c59c5b1f26f0f4ed6a659bf1f38ef886
D3rKlabauter <68585996+D3rKlabauter@users.noreply.github.com>
2020-11-20 11:08:39 +0100

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -7,14 +7,16 @@ int percentageHumidity = 0;
 const int watering_time = 500; //measured in ms
 const int watering_thrashhold = 50;
 
-const int dry = 697;  // value for dry sensor
+const int dry = 704;  // value for dry sensor
 const int wet = 339; // value for wet sensor
-bool watering = false;
+//bool watering = false;
 bool watered_today = false;
 
 void setup()
 { 
   Serial.begin(9600);
+  pinMode(water_tank,OUTPUT);
+  digitalWrite(water_tank, LOW);
 }
 
 void loop()
@@ -26,19 +28,22 @@ void loop()
 
   percentageHumidity = map(soilMoistureRaw, wet, dry, 100, 0); // More info: https://www.arduino.cc/reference/en/language/functions/math/map/
 
-  if ((percentageHumidity < watering_thrashhold) and (watered_today = false)){
+  if ((percentageHumidity < watering_thrashhold) and (watered_today == false)) {
 
-    digitalWrite(water_tank, HIGH);
-    delay(watering_time);
-    digitalWrite(water_tank, LOW);
-    watering = true;
+      digitalWrite(water_tank, HIGH);
+      delay(watering_time);
+      digitalWrite(water_tank, LOW);
+      watered_today = true;
+      Serial.print("ON"); 
   }
 
   else {
 
     Serial.print("watered today = ");
     Serial.print(watered_today);
+    Serial.print("OFF");
   }
+  
 
   Serial.print("(");
   Serial.print(soilMoistureRaw);
